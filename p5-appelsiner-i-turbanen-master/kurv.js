a51f42e2fad6a4421b082c34b52c81a5cbb7682b
Carl Gustav Bjergaard Aggeboe <gustav@eucnvs-3627.eucnvs.local>
2019-08-13 12:39:05 +0200

    ljkhjghfg

---
+++ p5-appelsiner-i-turbanen-master/kurv.js
@@ -0,0 +1,61 @@
+/*
+ * Dette script definerer klassen Kurv
+*/
+
+function Kurv(x, y, bredde, dybde, speed) {
+    /* Den første del af funktionen er en "konstruktør".
+     * Den tager parametrene og konstruerer et nyt objekt 
+     * ud fra dem. Værdierne huskes som hørende til netop 
+     * dette objekt ved hjælp af nøgleordet this
+     */
+    
+    this.x = x;
+    this.y = y;
+    this.bred = bredde;
+    this.dyb = dybde;
+    this.speed = speed;
+    this.col = [250,230,150];
+
+    this.tegn = function() {
+        fill(this.col);
+        rect(this.x, this.y, this.bred, this.dyb);
+    }
+
+    this.move = function(dir) {
+        if (dir == "up") {
+            this.y -= this.speed;
+            if (this.y < 0) {
+                this.y = 0;
+            }
+        }
+        if (dir == "down") {
+            this.y += this.speed;
+            if (this.y > height-this.dyb) {
+                this.y = height - this.dyb;
+            }
+        }
+        if (dir == "left") {
+            this.x -= this.speed;
+            if (this.x < 0) {
+                this.x = 0;
+            }
+        }
+        if (dir == "right") {
+            this.x += this.speed;
+            if (this.x > width-this.bred) {
+                this.x = width - this.bred;
+            }
+        }
+    }
+
+    this.grebet = function(xa, ya, ra) {
+        var tolerence = 5;
+        if ((ya < this.y+tolerence && ya > this.y-tolerence) && xa > this.x+ra && xa < this.x+this.bred-ra) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
